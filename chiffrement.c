f55aa391068ea9c88242bd4cf59756f80ff285b5
Julian Royet <julianroyet@free.fr>
2020-12-17 15:16:50 +0100

    chiffrement UAL

--- chiffrement.c
+++ chiffrement.c
@@ -9,6 +9,39 @@
 /* Exemples de cl�s: (565, 283189) , (140313, 283189) */
 /*                   (13,77), (37 77) */
 
+typedef long int l_int;
+
+l_int chiffrement(l_int m, l_int exp, l_int n){
+	l_int code=1;
+	while (exp>0)
+	{
+		if ((exp&1)>0) 
+		{
+			code= (code * m)%n;
+		}	
+		exp=exp>>1;
+		m= (m*m)%n;
+	}
+	return code;
+}
+l_int chiffrementUAL(l_int m, l_int exp, l_int n){
+	l_int code=1;
+	while (exp > 0)
+	{
+		if ((exp&1)>0)
+		{
+			code = (code * m);
+			while (code-n > 0)
+				code = code-n;
+		}
+		
+		exp=exp>>1;
+		m= (m*m);
+		while(m-n > 0)
+			m = m-n;
+	}
+	return code;
+}
 int main(int argc, char * argv[])
 {
     unsigned long int m,exp,n;
@@ -19,16 +52,7 @@ int main(int argc, char * argv[])
     sscanf(argv[2], "%ld", &exp);
     sscanf(argv[3], "%ld", &n);
     
-	code=1;
-	while (exp>0)
-	{
-		if ((exp&1)>0) 
-		{
-		code= (code * m)%n;
-		}	
-		exp=exp>>1;
-		m= (m*m)%n;
-	}
+	code = chiffrementUAL(m, exp, n);
 	printf("Resultat chiffrement %ld\n",  code);
 	return 0;
 }
